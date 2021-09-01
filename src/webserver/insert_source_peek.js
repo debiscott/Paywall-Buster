0d0eb5d6cecbfe1e2c1969711ba79aa6ba86d8de
Debi <4121172+debiscott@users.noreply.github.com>
2021-08-31 20:04:37 -0400

    Update insert_source_peek.js

--- src/webserver/insert_source_peek.js
+++ src/webserver/insert_source_peek.js
@@ -1,11 +1,12 @@
-  //a call to the function insert_source_peek (<script>insert_source_peek();</script>) is in the footer; without a call to this function the user will not be offered the chance to peek
+//a call to the function insert_source_peek (<script>insert_source_peek();</script>) is in the footer of dynamic pages; without a call to this function the user will not be offered the chance to snapshot the remote site. 
+
   function insert_source_peek () {
     var container = document.getElementById ("passcanonical");
     var id = container.innerHTML;  
     var deep_look = window.location.href ;
 
     var sendthis =  "'" + deep_look + "'" + "," + "'" + id + "'";
-    container.innerHTML = "<form id='breakpaywall' name='breakpaywall' onsubmit='return false'><input type=hidden value='" + deep_look + "'><input type=submit id=\"mybutton\" name=\"mybutton\" value=\"paywall buster.v.01\" onclick=\"call_pk_function(\'" + deep_look + "," + id + "\');\"></form>";
+    container.innerHTML = "<form id='breakpaywall' name='breakpaywall' onsubmit='return false'><input type=hidden value='" + deep_look + "'><input type=submit id=\"mybutton\" name=\"button\" value=\"paywall buster.v.01\" onclick=\"call_pk_function(\'" + deep_look + "," + id + "\');\"></form>";
     container.style.display = 'block';
     container.style.visibility = 'visible';
   }
